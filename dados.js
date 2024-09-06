b0dd423e02b07aa57c30494fff652af1476fedc8
RicardoSilva13 <ricardogomessilva99@gmail.com>
2024-09-05 23:03:52 -0300

    fazendo a base de dados em JavaScript

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+      tropa: "P.E.K.K.A",
+      descricao: "PEKKA é uma tropa imponente e poderosa que se tornou um símbolo de Clash of Clans. Sua força bruta e durabilidade a tornam uma escolha popular entre os jogadores que buscam destruir bases inimigas com eficiência",
+      link: "https://clashofclans.fandom.com/wiki/P.E.K.K.A"
+    },
+    {
+      tropa: "Bárbaro",
+      descricao: "Os Bárbaros são as primeiras tropas que os jogadores conhecem em Clash of Clans. Apesar de serem lentos e com pouca vida, eles são excelentes para distrair defesas e abrir caminho para outras tropas mais poderosas.",
+      link: "https://clashofclans.fandom.com/wiki/Barbarian"
+    },
+    {
+      tropa: "Corredor",
+      descricao: "O Corredor é uma tropa rápida e poderosa que ataca construções de forma rápida e eficiente. Ele é ideal para destruir defesas aéreas e alcançar o Alvo Principal rapidamente.",
+      link: "https://clashofclans.fandom.com/wiki/Hog_Rider"
+    }
+  ];
