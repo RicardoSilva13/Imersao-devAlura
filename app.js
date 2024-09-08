645baec4dd7e40266c632b80c8613d21a077f5f5
RicardoSilva13 <ricardogomessilva99@gmail.com>
2024-09-08 09:11:30 -0300

    Finalizando o banco de dados e dando função ao botão

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log(dados);
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+
+let resultados = ""
+
+for (let dado of dados) {
+    resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="${dado.link}"target="_blank">
+                ${dado.tropa}
+                </a>
+            </h2>
+                <p class="descricao-meta"> ${dado.descricao}
+                <p>
+            <a href="${dado.link}" target="_blank">Saiba mais
+            </a>
+        </div> `
+};
+section.innerHTML = resultados;
+}
--- dados.js
+++ dados.js
@@ -7,11 +7,36 @@ let dados = [
     {
       tropa: "Bárbaro",
       descricao: "Os Bárbaros são as primeiras tropas que os jogadores conhecem em Clash of Clans. Apesar de serem lentos e com pouca vida, eles são excelentes para distrair defesas e abrir caminho para outras tropas mais poderosas.",
-      link: "https://clashofclans.fandom.com/wiki/Barbarian"
+      link: "https://clashofclans.fandom.com/wiki/Barbarian",
     },
     {
       tropa: "Corredor",
       descricao: "O Corredor é uma tropa rápida e poderosa que ataca construções de forma rápida e eficiente. Ele é ideal para destruir defesas aéreas e alcançar o Alvo Principal rapidamente.",
       link: "https://clashofclans.fandom.com/wiki/Hog_Rider"
+    },
+    {
+      tropa: "Arqueira",
+      descricao: "A Arqueira é uma tropa de longo alcance que ataca inimigos de longe com suas flechas. Ela é uma ótima opção para eliminar defesas aéreas e tropas de apoio.",
+      link: "https://clashofclans.fandom.com/wiki/Archer"
+    },
+    {
+      tropa: "Mago",
+      descricao: "O Mago é uma tropa mágica que ataca múltiplos alvos ao mesmo tempo. Seus feitiços poderosos podem destruir grupos de tropas e construções rapidamente.",
+      link: "https://clashofclans.fandom.com/wiki/Wizard"
+    },
+    {
+      tropa: "Dragão",
+      descricao: "O Dragão é uma tropa aérea poderosa que causa dano em área e pode destruir quase qualquer coisa em seu caminho. Ele é uma ótima opção para ataques devastadores.",
+      link: "https://clashofclans.fandom.com/wiki/Dragon"
+    },
+    {
+      tropa: "Valquíria",
+      descricao: "A Valquíria é uma tropa poderosa que gira em torno de si mesma, atacando múltiplos inimigos ao mesmo tempo. Ela é excelente para destruir grupos de tropas e construções.",
+      link: "https://clashofclans.fandom.com/wiki/Valkyrie"
+    },
+    {
+      tropa: "Bebê Dragão",
+      descricao: "O Bebê Dragão é uma versão menor e mais rápida do Dragão. Ele é ideal para atacar alvos aéreos e causar danos em área. Quando não está perto de outras unidades aéreas, os Bebês Dragões ficam furiosos e ganham bônus de dano e velocidade de ataque ",
+      link: "https://clashofclans.fandom.com/wiki/Baby_Dragon"
     }
   ];
